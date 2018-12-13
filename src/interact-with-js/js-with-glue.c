dc0cc0cebf04d309af005b89c369c82e1255f2c9
daizhaohui <daizhaohui@wanda.cn>
2018-12-13 17:04:06 +0800

    ok

---
+++ src/interact-with-js/js-with-glue.c
@@ -0,0 +1,104 @@
+/*
+ * This file interacts with the canvas through imported functions.
+ * It moves a blue rectangle diagonally across the canvas
+ * (mimics the SDL example).
+ */
+#include <stdbool.h>
+#include <emscripten.h>
+
+#define BOUNDS 255
+#define RECT_SIDE 50
+#define BOUNCE_POINT (BOUNDS - RECT_SIDE)
+
+bool isRunning = true;
+
+typedef struct Rect
+{
+  int x;
+  int y;
+  char direction;
+} Rect;
+
+struct Rect rect;
+
+/*
+ * Updates the rectangle location by 1px in the x and y in a
+ * direction based on its current position.
+ */
+EMSCRIPTEN_KEEPALIVE
+void updateRectLocation()
+{
+  // Since we want the rectangle to "bump" into the edge of the
+  // canvas, we need to determine when the right edge of the
+  // rectangle encounters the bounds of the canvas, which is why
+  // we're using the canvas width - rectangle width:
+  if (rect.x == BOUNCE_POINT)
+    rect.direction = 'L';
+
+  // As soon as the rectangle "bumps" into the left side of the
+  // canvas, it should change direction again.
+  if (rect.x == 0)
+    rect.direction = 'R';
+
+  // If the direction has changed based on the x and y
+  // coordinates, ensure the x and y points update
+  // accordingly:
+  int incrementer = 1;
+  if (rect.direction == 'L')
+    incrementer = -1;
+  rect.x = rect.x + incrementer;
+  rect.y = rect.y + incrementer;
+}
+
+EM_JS(void, js_clear_rect, (), {
+  var canvas = document.querySelector('#myCanvas');
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#ff0000';
+  ctx.fillRect(0, 0, 255, 255);
+});
+
+EM_JS(void, js_fill_rect, (int x, int y, int width, int height), {
+  var canvas = document.querySelector('#myCanvas');
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#0000ff';
+  ctx.fillRect(x, y, width, height);
+});
+
+/*
+ * Clear the existing rectangle element from the canvas and draw a
+ * new one in the updated location.
+ */
+EMSCRIPTEN_KEEPALIVE
+void moveRect()
+{
+  js_clear_rect(0);
+  updateRectLocation();
+  js_fill_rect(rect.x, rect.y, RECT_SIDE, RECT_SIDE);
+}
+
+EMSCRIPTEN_KEEPALIVE
+bool getIsRunning()
+{
+  return isRunning;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void setIsRunning(bool newIsRunning)
+{
+  isRunning = newIsRunning;
+  EM_ASM({
+    var newStatus = $0 ? 'Running' : 'Paused';
+    document.querySelector('#runStatus').innerHTML = newStatus;
+  },
+         isRunning);
+}
+
+EMSCRIPTEN_KEEPALIVE
+void init()
+{
+  rect.x = 0;
+  rect.y = 0;
+  rect.direction = 'R';
+  setIsRunning(true);
+  emscripten_run_script("console.log('Initializing rectangle...')");
+}
